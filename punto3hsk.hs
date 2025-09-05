966fb0fa9fe9fd34a66d13606ec24a6dd6723d32
Felipe Calcetero <felicalce78@gmail.com>
2025-09-05 00:07:09 -0500

    Update punto3hsk.hs

--- punto3hsk.hs
+++ punto3hsk.hs
@@ -1,6 +1,8 @@
+promedio :: [Float] -> Float
+promedio = (\xs -> (foldr (+) 0 xs) / fromIntegral (length xs))
 
-
-promedio :: [Double] -> Double
-promedio xs = sum xs / fromIntegral (length xs)
-
-
+main :: IO ()
+main = do
+    let notas = [5.0, 4.8, 2.9, 5.0]
+    putStrLn ("Notas: " ++ show notas)
+    putStrLn ("Promedio: " ++ show (promedio notas))
